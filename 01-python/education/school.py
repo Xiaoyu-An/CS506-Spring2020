d03aabc3693d9f682a48c3282137cf037f98bce9
Xiaoyu An <xiaoyu@dhcp-wifi-8021x-155-41-86-165.bu.edu>
2020-01-27 17:35:32 -0500

    add road

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,10 @@
 def draw_school():
     print("school not found")
+    print("......^......")
+    print("...../.\.....")
+    print("____.....____")
+    print("|...........|")
+    print("|...........|")
+    print("|....____...|")
+    print("|____|..|___|")
     return
